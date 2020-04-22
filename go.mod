724129f42c39ff44658a6709f57cb088988a96f3
mark <mark.poling@gmail.com>
2020-04-22 19:17:13 -0400

    Testing rudimentary validation

--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module github.com/gojou/contacts
 
 require (
 	cloud.google.com/go/firestore v1.2.0
+	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/gorilla/mux v1.7.4
+	github.com/leodido/go-urn v1.2.0 // indirect
 	golang.org/x/tools v0.0.0-20200417140056-c07e33ef3290 // indirect
+	gopkg.in/go-playground/validator.v9 v9.31.0 // indirect
 )
--- pkg/handlers/contact.go
+++ pkg/handlers/contact.go
@@ -1,21 +1,35 @@
 package handlers
 
 import (
+	"errors"
 	"html/template"
 	"log"
 	"net/http"
-
-	"github.com/gojou/contacts/pkg/handlers/util"
 )
+
+// FormData stores field information in the case of a POST failure
+type FormData struct {
+	ID         string
+	FirstName  string
+	LastName   string
+	BirthYear  string
+	BirthMonth string
+	BirthDay   string
+	Email      string
+}
+
+// Data for use in the form
+var data FormData
 
 //Contact displays the contact page
 func Contact(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Entering the Contact form %s", data.ID)
 	page := template.Must(template.ParseFiles(
 		"static/html/_base.html",
 		"static/html/contact.html",
 	))
 
-	util.Xecute(page, w, r)
+	page.Execute(w, data)
 
 }
 
@@ -25,15 +39,32 @@ func ContactPost(w http.ResponseWriter, r *http.Request) {
 
 	for key, value := range r.Form {
 		if value[0] != "" {
-			log.Printf("key = %s -- value = %s", key, value)
+			log.Printf("key = %s -- value = %s", key, value[0])
 		}
 	}
 
+	data.ID = r.FormValue("id")
+	log.Printf("Final test, structure formdata.id = %s", data.ID)
 	page := template.Must(template.ParseFiles(
 		"static/html/_base.html",
 		"static/html/contact.html",
 	))
 
-	page.Execute(w, nil)
+	// validate returns a slice of errors if the form contains any invalid
+	// strings.
+	// TODO: Update contact.html to populate the VALID data from the original
+	// form submission.
+	if len(validate(data)) > 0 {
+		page.Execute(w, data)
+	} else {
+		// TODO: save the form data to the database
+		// e:= repository.Commit(data)
+		// if e!=nil {} etc
+		data = FormData{}
+		page.Execute(w, data)
+	}
+}
 
+func validate(data FormData) (errs []error) {
+	return append(errs, errors.New("danger will robinson"))
 }
